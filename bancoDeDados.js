6985a1b64ab304ccccbabf2478c47a926bb4dc78
abeamonteiro <abeatrizbmonteiro@gmail.com>
2025-04-15 19:40:15 -0300

    conexao com o BD funcionando

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose') //trazendo o pacote mongoose
         try { //tente isto: (try/catch)
             console.log('Conexão com o banco de dados iniciou')
 
-            await mongoose.connect('mongodb+srv://abeatrizbmonteiro:jsD5BuzNwEvJEwUc@cluster0.bmiuv1l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0') 
+            await mongoose.connect('mongodb+srv://beatrizbmonteiro:SVzgJZc5TR1yhqD8@cluster1.ixj0acg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1') 
         // o await serve para que o node nao pare de atender as outras pessoas usuarias enquanto trabalha aqui
             console.log('Conexão com o banco de dados feita com sucesso!')
         } catch(erro) { //caso não consiga, pegue o erro
