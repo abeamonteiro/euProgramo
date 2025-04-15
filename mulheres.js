d2ba3d1923f72cacc3ce3154f47d7e03109ed96d
abeamonteiro <abeatrizbmonteiro@gmail.com>
2025-04-15 20:35:03 -0300

    criada função GET no BD

--- mulheres.js
+++ mulheres.js
@@ -5,33 +5,21 @@ const { v4: uuidv4 } = require('uuid')
 const conectaBanco = require('./bancoDeDados') //ligando ao arquivo bancoDeDados
 conectaBanco() //chamando a função que conecta o Banco de Dados
 
+const Mulher = require('./mulherModel')
 const app = express() // aqui estou iniciando o app
 app.use(express.json()) //tratando as requisições; a partir disso elas tambem estarão no json
 const porta = 3333 // aqui estou criando a porta
 
-const mulheres = [ //aqui estou criando lista inicial de mulheres
-    {
-        id: '1',
-        nome: 'Simara Conceição',
-        imagem: 'https://github.com/simaraconceicao.png',
-        minibio: 'Desenvolvedora e Instrutora'
-    },
-    {
-        id: '2',
-        nome: 'Iana Chan',
-        imagem: 'https://bit.ly/33CXBqP',
-        minibio: 'Fundora da Programaria'
-    }
-]
-
 //FUNÇÃO GET
-function mostraMulher(request, response) {
-    response.json({       // uma forma de enviar pela internet, várias informaçoes organizadas, um objeto; no send, apenas uma info
-        nome: 'Ana Beatriz Monteiro',
-        imagem: 'https://lh3.googleusercontent.com/a/ACg8ocKgWCiGVd36YDmTARVvXvdbokkUZ9xbaD3zOFPr2fz5gVsSo0qHOg=s288-c-no',
-        minibio: 'Assistente Social e Desenvolvedora de Software'
-    })
-}
+ async function mostraMulher(request, response) {
+    try {
+        const mulheresVindasDoBD = await Mulher.find()
+
+        response.json(mulheresVindasDoBD)
+    } catch(erro) {
+        console.log(erro)
+    }
+ }
 
 //FUNÇÃO POST
 function criaMulher(request, response) {
@@ -95,5 +83,4 @@ app.use(router.delete('/mulheres/:id', deletaMulher)) //configurei rota DELETE /
 function mostraPorta() {
     console.log ("Servidor criado e rodando na porta", porta)
 }
-
 app.listen(porta, mostraPorta) // servidor ouvindo a porta
